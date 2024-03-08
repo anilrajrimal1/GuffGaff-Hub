4b951ffcbb056e86ba968d9d08c8ea32c8ca4919
sandesh4313 <sandeshshrestha4313@gmail.com>
2024-03-08 15:09:59 +0545

    Encryption messages

--- ChatApp-Backend/src/main/java/com/chatapp/model/Message.java
+++ ChatApp-Backend/src/main/java/com/chatapp/model/Message.java
@@ -2,11 +2,8 @@ package com.chatapp.model;
 
 import java.time.LocalDateTime;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
+import com.chatapp.config.AesEncryptor;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -20,6 +17,7 @@ public class Message {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer id;
+	@Convert(converter = AesEncryptor.class)
 	private String content;
 	
 	private LocalDateTime timestamp;
